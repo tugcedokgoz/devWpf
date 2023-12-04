f8e645630a46b8e8f2e7c7ac819e5444f49a8a47
Tuğçe Dokgöz <tugcedokgoz201@gmail.com>
2023-12-04 13:10:55 +0300

    .

--- obj/Debug/net7.0-windows/MainWindow.g.i.vb
+++ obj/Debug/net7.0-windows/MainWindow.g.i.vb
@@ -77,7 +77,7 @@ Partial Public Class MainWindow
             Return
         End If
         _contentLoaded = true
-        Dim resourceLocater As System.Uri = New System.Uri("/WpfDevExpress1;component/mainwindow.xaml", System.UriKind.Relative)
+        Dim resourceLocater As System.Uri = New System.Uri("/WpfDevExpress1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative)
         
         #ExternalSource("..\..\..\MainWindow.xaml",1)
         System.Windows.Application.LoadComponent(Me, resourceLocater)
